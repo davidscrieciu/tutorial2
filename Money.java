e58959b94226db5212eafced7e3a826e3792aa8a
davidscrieciu <davidscrieciu@cmail.carleton.ca>
2017-07-19 20:07:28 -0400

    Added new file to repository

--- Money.java
+++ Money.java
@@ -11,19 +11,65 @@ public class Money{
   
   public Money(){
 		// create an object with zero dollars and cents.
+		dollars = 0;
+		cents = 0;
   }
-  
+ 
   public Money(int c){
 		// create an object with c cents
 		// (adjusting dollars and cents so that 0<=cents<=99)
+		//dollars = 0;
+		//if (c<100){
+		//cents = c;
+		//}while (c>=100){
+		//c = c - 100;
+		//dollars += 1;
+		//}
+		//cents = c;
+		dollars = c/100;
+		cents = c % 100;
   }
   
   public Money(int d, int c){
 		// create an object with d dollars and c cents
 		// (adjusting dollars and cents so that 0<=cents<=99)
+		while (c>=100){
+		c = c - 100;
+		d += 1;
+		}
+		cents = c;
+		dollars = d;
   }
   
-  
+  public void add(int c){
+	// adds c cents to the current value 
+	int total = dollars * 100 + cents + c;
+	dollars = total/100;
+	cents = total % 100;
+  }
+
+  public void add(int d, int c){
+  // adds d dollars and c cents to the current value
+  	int total = (dollars + d) * 100 + cents + c;
+	dollars = total/100;
+	cents = total % 100;
+  }
+
+  public int remove(int c){
+  // removes c cents from current value if current 
+  // value is large enough. Otherwise, removes as much as it can.
+  // Returns the actual amount of cents removed (may be > 100)
+  	
+	int total = dollars * 100 + cents;
+	if (c>=100){
+	 dollars = 0;
+	 cents = 0;
+	}else{
+		total = total - c;
+	}
+	return total;
+  }
+
   
   /** 
    * Returns a String representation of the value of the current object. 
